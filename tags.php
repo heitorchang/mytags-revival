a79cbb35a425380b2506208b1989b1fd176ab9c7
Heitor Chang <heitorchang@gmail.com>
2020-04-16 15:11:55 -0300

    ignore empty tags

--- tags.php
+++ tags.php
@@ -3,7 +3,7 @@
 include "header.php";
 include "db.php";
 
-$getTags_result = $dbh->query("SELECT `name` FROM `tag` ORDER BY `name`");
+$getTags_result = $dbh->query("SELECT t.name FROM tag t INNER JOIN post p ON p.tags LIKE CONCAT('%', t.name, '%') GROUP BY name ORDER BY name");
 
 foreach($getTags_result as $row) {
   echo '<a class="taglink taglink_all" href="listbytitle.php?tagname=' . $row['name'] . '">' . $row['name'] . "</a> ";
