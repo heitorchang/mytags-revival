0aae9cec08cbbccc3eebfb2480dd93c2ca7f580a
crystal <crystal@crystal.nfshost.com>
2021-06-19 22:59:14 +0000

    separate tags by letter

--- tags.php
+++ tags.php
@@ -5,6 +5,12 @@ include "db.php";
 
 $getTags_result = $dbh->query("SELECT t.name FROM tag t INNER JOIN post p ON p.tags LIKE CONCAT('%|', t.name, '|%') GROUP BY name ORDER BY name");
 
-foreach($getTags_result as $row) {
+$currentLetter = '-';
+
+foreach($getTags_result as $row) { 
+  if ($row['name'][0] != $currentLetter) {
+    echo '<br>&nbsp;&nbsp;&nbsp;';
+    $currentLetter = $row['name'][0];
+  }
   echo '<a class="taglink taglink_all" href="listbytitle.php?tagname=' . $row['name'] . '">' . $row['name'] . "</a> ";
 }
